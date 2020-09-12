d36b69ec42186e5d52393101dbd276566d39df79
Uanderson Santarem <59483858+uanderns@users.noreply.github.com>
2020-09-12 18:51:42 -0300

    add file js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function enviar(){
+
+var nome = document.getElementById('name').value;
+var marca = document.getElementById('marca').value;
+var fabricante = document.getElementById('fabricante').value;
+var preco = document.getElementById('preco').value;
+var codigo = document.getElementById('codigo').value;
+var estoque = document.getElementById('estoque').value;
+
+console.log(nome);
+
+}
+
+
